0f94c9156d6cac188af0481c64e1fdf588ee5174
pietervanheijningen <pietervanheijningen@outlook.com>
2018-09-06 20:08:14 +0200

    redid routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
+// login routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
-Route::get('/', 'HomeController@index')->name('home');
-Route::resource('/books','BookController');
-Route::post('/books/upload','BookController@upload');
+// user needs to be logged in to access this
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('/books','BookController');
+    Route::post('/books/upload','BookController@upload');
+});
