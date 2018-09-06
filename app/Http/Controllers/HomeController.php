b3b350b09a7cba24c0cc1e1fd20c58e3aa8c60a4
pietervanheijningen <pietervanheijningen@outlook.com>
2018-09-06 13:21:37 +0200

    Added overview of all books to homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Book;
 
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+    	$books = Book::all();
+
+        return view('home',compact('books'));
     }
 }
