e1d7f12f47e35c436ee0de9333926a7e68ac84b2
wesley <wn.sientje@gmail.com>
2018-09-20 12:13:58 +0200

    loans

--- database/seeds/BookRealSeeder.php
+++ database/seeds/BookRealSeeder.php
@@ -18,23 +18,27 @@ class BookRealSeeder extends Seeder
 				'content'   => 'Een boek vol tips om direct te gebruiken op school. Handig voor als je net begint in het onderwijs. En minstens net zo praktisch voor wie al een leven voor de klas staat. Tips bijvoorbeeld om de vaart in je les te houden, samen te werken met collega\'s of lastige onderwerpen op een open manier te bespreken. Succes verzekerd, toch?
             Wás het maar zo simpel. Tips zijn soms heel persoonlijk. Probeer zelf de tips uit, speel er mee en bespreek ze met collega\'s. Dan ontdek je wat deze tips voor jou betekenen en hoe jij er plezier van kan hebben in je werk.',
 				'coverpath' => '/bookcovers/276_tips_voor_leerkrachten.jpg',
+                'isbn'      => '',
 			], [
 				'title'     => 'Autisme in het middelbaar beroepsonderwijs',
 				'author'    => 'Esther van de Ven',
 				'content'   => 'Studenten met autisme verwerken informatie anders en hebben daardoor meer behoefte aan duidelijkheid, voorspelbaarheid  en concrete aansturing dan andere studenten. Dit botst met de nadruk die het middelbaar beroepsonderwijs (mbo) legt op het vermogen tot zelfreflectie, zelfstandigheid en flexibiliteit van studenten. 
                 Dit boek is bedoeld voor begeleiders, docenten en zorgcoördinatoren in het mbo, die te maken hebben met studenten met autisme. In het voortgezet onderwijs kan het boek ingezet worden ter voorbereiding op de overstap naar het mbo. Ook voor ouders kan het boek een bron van informatie zijn over de gang van zaken én de mogelijkheden op de school.',
 				'coverpath' => '/bookcovers/autisme_in_het_middelbaarberoepsonderwijs.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'Beter leerklimaat in het MBO',
 				'author'    => 'Annette Breaux en Todd Whitaker',
 				'content'   => 'Beter leerklimaat in het MBO is geschreven voor iedereen die voor de klas staat in het middelbaar beroepsonderwijs. Dit boek beoogt niet om perfecte docenten te maken; zulke docenten bestaan namelijk niet. Het helpt ook niet om perfecte studenten te vormen; want die bestaan evenmin. Deze uitgave reikt wel veertig praktische en direct toepasbare tips aan om het gedrag van studenten in het MBO te verbeteren. En het mooie is: als hun gedrag verbetert, verbeteren hun prestaties ook. Wie de tips uit dit boek ter harte neemt, zal bovendien z\'n manier van lesgeven structureel verbeteren.',
 				'coverpath' => '',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'betrokkenheid! De sleutel tot beter leren',
 				'author'    => 'Robert J. Marzano En Debra J. Pickering Met Tammy Heflebower',
 				'content'   => 'In dit boekje maak je kennis met die vier factoren. Deze beknopte uitgave is afgeleid van het boek Betrokkenheid!, waarin Marzano de vier factoren verder uitdiept en zestien concrete lesstrategieën geef om de betrokkenheid van de leerlingen aanzienlijk te vergroten.',
 				'coverpath' => '/bookcovers/betrokkenheid.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'Coöperatief vergaderen voor professionals',
 				'author'    => 'Spencer Kagan, Dook Kopmels en Betty de Jaeger',
 				'content'   => 'Woont u graag vergaderingen bij? Leert u op de vergadering uw collega\'s beter kennen en gaat u ze meer waarderen? Zorgt het gesprek op de vergadering ervoor dat iedereen zijn werk beter gaan doen? Kunt u tijdens de vergadering meewerken aan het nemen van besluiten? Brengt u in de vergaderingen alles wat u weet over effectief communiceren, leren en samenwerken in de praktijk? 
@@ -49,7 +53,8 @@ Ja, het kan beter, door echt samen te vergaderen, coöperatief te vergaderen:
 Dat komt omdat de praktijk niet deugt. Het klinkt mooi en het is mooi, want het kan! \'\'Maar hoe dan?\'\' Door het toepassen van de uitgangspunten van coöperatief vergaderen.
  \'\'En dat betekent dan zeker dat ik voor iedere vergadering 4 uur voorbereiding kwijt ben?\'\' Nee! Uiteraard moet u even bedenken hoe u het gaat aanpakken. Maar als u na het lezen van dit boek de diverse mogelijkheden in uw hoofd heeft, is dat iets dat al vrij snel weinig extra tijd vergt. En dan gaat het meer over minuten dan over uren. Coöperatief Vergaderen ......echt iets voor professionals.',
 				'coverpath' => '/bookcovers/cooperatief_vergaderen_voor_professionals.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'Coöperatieve  Leerstrategieën Research, principes en de praktische uitwerking',
 				'author'    => 'Dr. Spencer Kagan',
 				'content'   => 'Beknopte uitgave coöperatieve leerstrategieën
@@ -63,25 +68,29 @@ Werken met coöperatieve leerstrategieën :
 Deze bewezen resultaten doen zich voor bij alle leeftijden, van peuterspeelzaal tot universiteit, op alle vakgebieden.
 Ervaar zelf hoe u met coöperatieve structuren het verschil kunt maken! Dit boekje is een beknopte weergave van Kagans uitgave coöperatieve leerstrategieën (Bazalt, 2010)',
 				'coverpath' => '/bookcovers/cooperatieve_leerstrategieen.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'CreaLucion',
 				'author'    => 'Coen Free',
 				'content'   => 'Vijfhonderd jaar geleden was er in het middeleeuwse Europa een grote beroering, dankzij Jheronimus Bosch en Erasmus. Er ontpopte zich allerlei nieuwe gedachten en levensvormen waardoor een geheel nieuw mens- en wereldbeeld ontstond. In het boek CREALUCION doet Coen Free verslag van zijn boeiende zoektocht naar de heilige graal: gedroomd onderwijs dat ruimte biedt aan een optimale talentontwikkeling voor alle kinderen.',
 				'coverpath' => '/bookcovers/crealucion.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De ambachts man De mens als maker',
 				'author'    => 'Richard Sennett',
 				'content'   => 'Volgens arbeidssocioloog Richard Sennett is ambachtelijkheid meer dan louter vakmanschap. Ambachtelijkheid staat voor een blijvende, basale menselijke neiging: het verlangen om werk goed te doen omwille van het werk zelf, waardoor we vaardigheden ontwikkelen en gericht zijn op het werk in plaats van op onszelf. In dit tot nadenken stemmende boek onderzoekt een van de grootste sociologen van deze tijd het werk van de ambachtsman in heden en verleden, vergelijkt hij de diepe verbanden tussen materieel bewustzijn en ethische waarden, en ondergraaft hij algemeen aanvaarde ideeën over wat bijdraagt aan goede arbeid.
 Sennett reist in De ambachtsman door rijd en ruimte: van de klassieke Romeinse stenenmakers naar de goudsmeden van de Renaissance, van de drukpersen van de Verlichting in Parijs en de Industriële Revolutie in Londen naar de moderne wereld. De ambachtsman is een briljante cultuurgeschiedenis over onze verhouding tot ons werk.',
 				'coverpath' => '/bookcovers/de_ambachts_man.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De Creatiespiraal',
 				'author'    => 'Marinus Knoope',
 				'content'   => 'Zoals de bloesem in de lente een voorbode is van het fruit in de zomer, zo zijn wensen en verlangens van mensen een voorbode van hetgeen ze later kunnen realiseren.
 Zoals fruitbomen over precies die kwaliteiten beschikken die ze in staat stellen de eigen fruitsoort te creëren, zo beschikken mensen over precies die kwaliteiten die ze nodig hebben om hun eigen wensen en verlangens te kunnen realiseren.
 Zoals er in de natuur een vaste route is van bloesem naar vrucht en van zaad naar oogst, zo is er in de natuur ook een welbepaalde weg van wens naar werkelijkheid. Die weg wordt in dit boek beschreven.',
 				'coverpath' => '/bookcovers/de_creatiespiraal.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De impact van leren zichtbaar maken Nederlandse vertaling van Visible Learning',
 				'author'    => 'John Hattie',
 				'content'   => 'De impact van Leren zichtbaar maken (Visible Learning) van John Hattie, bevat het resultaat van onderzoek waarbij miljoenen leraren en leerling betrokken waren en dat gebaseerd is op de synthese van meer dan 800 meta-analyses. 
@@ -103,7 +112,8 @@ In dit boek krijg je op zes domeinen verschillende bouwstenen die je kunnen help
 •	 vanuit de eigen onderwijscontext te kiezen voor wat het beste werkt.
 Dit boek bevat de meest actuele onderzoeksresultaten van \'Leren zichtbaar maken\'\'',
 				'coverpath' => '/bookcovers/de_impact_van_leren_zichtbaar_maken.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De kunst en wetenschap van het lesgeven',
 				'author'    => 'Robert J. Marzano',
 				'content'   => 'Tien vragen (en antwoorden) om uw lessen sterker te maken
@@ -121,7 +131,8 @@ In deze uitgave benadrukken we zowel de kunst als de wetenschap van het lesgeven
 9.	Hoe communiceer ik hoge verwachtingen voor alle leerlingen? 
 10.	Hoe ontwikkel ik goede lessen binnen een samenhangend geheel?',
 				'coverpath' => '/bookcovers/de_kunst_en_wetenschap_van_het_lesgeven.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De leerafdeling: Een specifieke vorm van werkplekleren',
 				'author'    => 'Niek van den Berg, Ad de Jongh, Ad Klous en Jan Streumer',
 				'content'   => 'In 2009 is in het kader van het doorbraakproject \'\'Werkplekken in de regio Rijnmond\'\' onderzoek gedaan naar de vormgeving en de effecten van drie leerafdelingen van deze regio.
@@ -129,7 +140,8 @@ In deze tweede brochure in de reeks \'\'Werkplekleren regio Rijnmond\'\'
 Zijn de resultaten van het onderzoek genomen.
 Aandacht wordt besteed aan de volgende Thema\'s. Argumenten voor en tegen de leerling, de vormgeving van de afdeling, de effecten van de leerafdeling en de vergelijking van de stage op de leerafdeling met de individuele stages.',
 				'coverpath' => '/bookcovers/de_leerafdeling.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De Regisseur Van je eigen leven',
 				'author'    => 'Abkader Chrifi',
 				'content'   => 'Voor vele mensen is het moeilijk om de regie over hun eigen leven te voeren. Dat komt voor een deel door het normen- en waardenpatroon dat wij hebben opgebouwd en dat ons wereldbeeld is gaan bepalen. Al onze activiteiten worden tegen deze, soms beperkende, patronen afgezet.
@@ -137,14 +149,16 @@ De huidige economische en sociaal-maatschappelijke crisis helpt ook al niet mee.
 De Regisseur reikt je een succesformule aan waarmee je de weg naar persoonlijk geluk en succes zult vinden. Abkader Chrifi beschrijft op een inspirerende en innemende manier hoe alle mensen in staat zijn de regie over hun leven te voeren en te worden wie ze willen zijn door een proces van zelfbewustzijn, positief denken, vertrouwen, passie en vastberadenheid. Het is aan jou om die kans te grijpen. Niet afwachtend en passief, maar actief, met het roer stevig in eigen handen en het vizier gericht op de toekomst die je zelf wenst te scheppen. Dat is regisseren pur sang.
 Abkader Chrifi heeft als trendsetter met zijn boeken, trainingen, workshops en lezingen over Empowerment in de nieuwe, veranderende westerse culturen al duizenden mensen weten te raken. Eerder schrijf hij "Het succes ligt op straat" en het autobiografische "Strijd van een vreemde". Chrifi is in Nederland en België een veelgevraagd spreker. Niet verwonderlijk: zijn onwaarschijnlijke succesverhaal is voor talloze mensen een bron van inspiratie. ',
 				'coverpath' => '/bookcovers/de_regisseur.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De rendements denker',
 				'author'    => 'Lucas Zandberg',
 				'content'   => 'Klassenstrijd in een leerfabriek: een actueel onderwijsdrama
 Robert Witteveen, pas afgestudeerd aan de lerarenopleiding Engels, begint vol goede moed aan zijn allereerste baan in het onderwijs. Op het ROC Leyderschans lijkt hij zijn roeping te hebben gevonden. Hij wint zijn leerlingen voor zich en groeit snel uit tot een geliefde, bevlogen leraar. Maar Roberts onderwijscarrière neemt een onverwachte wending met de komst van een resultaatgerichte sectormanager en de lancering van een megalomaan nieuwbouwproject door het College van Bestuur. 
 Lucas Zandberg putte voor deze roman inspiratie uit de actualiteit en zijn eigen jarenlange ervaring als docent.',
 				'coverpath' => '/bookcovers/de_rendements_denker.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'De 5 frustraties van teamwork',
 				'author'    => 'Patrick Lencioni',
 				'content'   => 'In de vijf frustraties van teamwork legt Patrick Lencioni de kern van samenwerking in teams bloot. Dat doet hij aan de hand van deze parabel, waarin Catherine Petersen als ceo een team moet leiden dat onderling zo in conflict is geraakt dat het de hele organisatie negatief beïnvloedt. Gaandeweg openbaren zich vijf grote frustraties waarmee teamleden worstelen ,frustraties die de samenwerking saboteren: gebrek aan vertrouwen, angst voor confrontatie, gebrek aan betrokkenheid afschuiven van verantwoordelijkheid en niet-resultaatgericht werken.
@@ -156,12 +170,14 @@ Lencioni laat middels dit verhaal zien hoe deze hindernissen ontstaan en hoe je 
 Patrick Lencioni staat aan het hoofd van zijn table Group en is een succesvol spreker en adviseur. Hij schreef eerder De vijf valkuilen voor managers en de drie symptomen van een rotbaan. 
 Van zijn boeken zijn in meer dan tien landen vertalingen verschenen.',
 				'coverpath' => '/bookcovers/de_5_frustraties_van_teamwork.jpg',
-			], [
+                'isbn'      => '',
+            ], [
 				'title'     => 'Didactiek en pedagogiek in het beroepsonderwijs',
 				'author'    => 'Rene van Kralingen',
 				'content'   => 'Dit boek gaat over pedagogisch en didactisch handboek voor instructeurs, onderwijsassistenten en docenten in het beroepsonderwijs. Dit complete en praktische boek leert je doordacht te handelen in de onderwijspraktijk. Het gaat over gevarieerde aspecten van het beroep, zoals: samenwerken met docenten en assistenten in een team, begeleiden van leerlingen of studenten, instructies geven, het overnemen van lessen, het vormen van studenten en het registeren van vorderingen. Het boek bevat praktische voorbeelden, casuïstiek en realistische opdrachten, uitgevoerd met tekeningen en kleurenfoto\'s.',
 				'coverpath' => '/bookcovers/didactiek.jpg',
-			],
+                'isbn'      => '',
+            ],
 		])
 		;
 	}
