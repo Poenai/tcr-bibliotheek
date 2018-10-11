bf418080dcd7bb595b96b0c3311cb42572c19128
pietervanheijningen <pietervanheijningen@outlook.com>
2018-10-11 15:01:10 +0200

    Improved loansfactory to select random books

--- database/factories/LoansFactory.php
+++ database/factories/LoansFactory.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 $factory->define(App\Loan::class, function () {
 	return [
 		'user_id'     => 1,
-		'book_id'     => 1,
+		'book_id'     => \App\Book::all()->random()->id,
 		'loan_date'   => Carbon::now(),
 		'return_date' => Carbon::now(),
 	];
