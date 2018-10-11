bb53ba943c63311026de4d67c797a475b283d6a1
pietervanheijningen <pietervanheijningen@outlook.com>
2018-10-11 15:46:56 +0200

    fixed stylesheet not loading in on subpages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="css/layout.css?v=1" rel="stylesheet">
+    <link href="/css/layout.css?v=1" rel="stylesheet">
 </head>
 <body>
 <div id="app">
