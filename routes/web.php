29724f6a38e45a482a2e66188e07092406f31ecd
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-06 10:46:12 +0100

    added admin middleware and improved the routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,19 @@ Auth::routes();
 
 // user needs to be logged in to access this
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/', 'HomeController@index')->name('home');
-    Route::resource('/books','BookController');
-    Route::post('/books/upload','BookController@upload');
-    Route::post('/books/search','BookController@search');
-    Route::resource('/loans','LoanController');
-    Route::post('/loans/search','LoanController@search');
+	Route::get('/', 'HomeController@index')->name('home');
+	Route::resource('/books', 'BookController')->only('index', 'show');
+	Route::post('/books/search', 'BookController@search');
 
+	// user also needs to be an admin
+	Route::group(['middleware' => ['admin']], function () {
+		Route::resource('/loans', 'LoanController');
+		Route::post('/loans/search', 'LoanController@search');
+		Route::post('/books/upload', 'BookController@upload');
+		Route::resource('/books', 'BookController')->except('index', 'show');
+
+		// temp test pages
+		Route::get('/userspage', 'UserController@index');
+		Route::get('/userspage/create', 'UserController@create');
+	});
 });
-
-
-// temp test pages
-Route::get('/userspage', 'UserController@index');
-Route::get('/userspage/create', 'UserController@create');
