5c667e61918a6680d83b3c85e13e6306a71813b4
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 00:30:18 +0100

    Added standard date to date input

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -14,7 +13,7 @@
             <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
             <input type="hidden" name="book_id" value="{{$book->id}}">
             <label for="loanDate">datum van uitgifte:</label>
-            <input type="date" id="loanDate" name="loan_date" value="<?= date('Y-m-d') ?>" required >
+            <input type="date" id="loanDate" name="loan_date" value="<?= date('Y-m-d') ?>" required>
             <label for="returnDate">datum van terugname:</label>
             <input type="date" id="returnDate" name="return_date" required>
             <input type="submit" value="Bestel" class="btn">
