7a9b6ffdb3cd46a4c5bb29a3fd8b94a9a9714198
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 00:28:33 +0100

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,20 @@ Auth::routes();
 // needs to be there otherwise it won't be registered.
 // still only admins can access it.
 Route::get('/books/create','BookController@create');
+Route::get('/books/store','LoanController@store');
 
 Route::group(['middleware' => ['auth']], function () {
 	Route::get('/', 'HomeController@index')->name('home');
 	Route::resource('/books', 'BookController')->only('index', 'show');
+    Route::resource('/loans', 'LoanController')->only('create','store');
 	Route::post('/books/search', 'BookController@search');
 
 	Route::group(['middleware' => ['admin']], function () {
-		Route::resource('/loans', 'LoanController');
+
 		Route::post('/loans/search', 'LoanController@search');
 		Route::post('/books/upload', 'BookController@upload');
+
+        Route::resource('/loans', 'LoanController')->except('create','store');
 		Route::resource('/books', 'BookController')->except('index', 'show');
 
 		// temp test pages
