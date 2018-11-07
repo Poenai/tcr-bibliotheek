8f35fa745f480b5637afc0ae2a165d1d73e850f5
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 00:21:14 +0100

    Added standard date for loans

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -14,7 +14,7 @@
             <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
             <input type="hidden" name="book_id" value="{{$book->id}}">
             <label for="loanDate">datum van uitgifte:</label>
-            <input type="date" id="loanDate" name="loan_date" required>
+            <input type="date" id="loanDate" name="loan_date" value="<?= date('Y-m-d') ?>" required >
             <label for="returnDate">datum van terugname:</label>
             <input type="date" id="returnDate" name="return_date" required>
             <input type="submit" value="Bestel" class="btn">
