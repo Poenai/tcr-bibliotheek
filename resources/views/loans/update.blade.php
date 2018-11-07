f6fa36a19535e14c1c27414d7e634b5e6c6c74b9
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 00:44:07 +0100

    more more more form control

--- resources/views/loans/update.blade.php
+++ resources/views/loans/update.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
     <form action="{{URL::asset('/loans'). '/'.$loan->id}} " method="post">
-        {{ csrf_field() }}
-        {{method_field('patch')}}
+        <div class="formcontainer">
+            {{ csrf_field() }}
+            {{method_field('patch')}}
 
-        <label for="book">Boek:</label>
-        <select id="book" name="book_id">
+            <label for="book">Boek:</label>
+            <select id="book" name="book_id" class="form-control">
                 <option value="{{$loanInfo[1]}}" selected="selected">{{$loanInfo[5]}}</option>
-            @foreach($books as $book)
-                <option value={{$book->id}}>{{$book->title}}</option>
-            @endforeach
-        </select>
-        <label for="user">persoon:</label>
-        <select id="user" name="user_id">
-            <option value="{{$loanInfo[0]}}">{{$loanInfo[4]}}</option>
-        @foreach($users as $user)
-                <option value={{$user->id}}>{{$user->name}}</option>
-            @endforeach
-        </select>
-        <label for="loanDate">datum van uitgifte:</label>
-        <input type="date" id="loanDate" name="loan_date">
-        <label for="returnDate">datum van terugname:</label>
-        <input type="date" id="returnDate" name="return_date">
-        <input type="submit" value="pas aan">
+                @foreach($books as $book)
+                    <option value={{$book->id}}>{{$book->title}}</option>
+                @endforeach
+            </select>
+            <label for="user">persoon:</label>
+            <select id="user" name="user_id" class="form-control">
+                <option value="{{$loanInfo[0]}}">{{$loanInfo[4]}}</option>
+                @foreach($users as $user)
+                    <option value={{$user->id}}>{{$user->name}}</option>
+                @endforeach
+            </select>
+            <label for="loanDate">datum van uitgifte:</label>
+            <input type="date" id="loanDate" name="loan_date" class="form-control">
+            <label for="returnDate">datum van terugname:</label>
+            <input type="date" id="returnDate" name="return_date"  class="form-control">
+            <input type="submit" value="pas aan" class="btn">
+        </div>
 
     </form>
 
