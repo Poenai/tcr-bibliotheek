6dd97f2ea7f147fc57f43709d8c911aaa514fa31
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 00:01:56 +0100

    Added requireds to form

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -7,10 +7,11 @@
         <div class="formcontainer">
             <label for="image">Voeg een cover foto toe:</label>
             <input type="file" name="image" id="image">
-            <input type="text" id="title" name="title" placeholder="titel">
-            <input type="text" id="author" name="author" placeholder="schrijver">
+            <input type="text" id="title" name="title" placeholder="titel" required>
+            <input type="text" id="author" name="author" placeholder="schrijver" required>
+            <input type="text" id="isbn" name="isbn" placeholder="ISBN nummer">
             <label for="content">Korte beschrijving:</label>
-            <textarea id="content" cols="70" rows="15" name="content"></textarea>
+            <textarea id="content" cols="70" rows="15" name="content" required></textarea>
             <input type="submit" class="btn" value="Opslaan">
         </div>
     </form>
