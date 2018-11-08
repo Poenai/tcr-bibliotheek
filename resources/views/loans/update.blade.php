8db5d5dfd31191f2661e5f545892b6d152f71648
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 15:11:22 +0100

    data fillin for loan edit

--- resources/views/loans/update.blade.php
+++ resources/views/loans/update.blade.php
@@ -7,23 +7,23 @@
             {{method_field('patch')}}
 
             <label for="book">Boek:</label>
-            <select id="book" name="book_id" class="form-control">
+            <select id="book" name="book_id" class="form-control" required>
                 <option value="{{$loanInfo[1]}}" selected="selected">{{$loanInfo[5]}}</option>
                 @foreach($books as $book)
                     <option value={{$book->id}}>{{$book->title}}</option>
                 @endforeach
             </select>
-            <label for="user">persoon:</label>
-            <select id="user" name="user_id" class="form-control">
+            <label for="user">lener:</label>
+            <select id="user" name="user_id" class="form-control" required>
                 <option value="{{$loanInfo[0]}}">{{$loanInfo[4]}}</option>
                 @foreach($users as $user)
                     <option value={{$user->id}}>{{$user->name}}</option>
                 @endforeach
             </select>
             <label for="loanDate">datum van uitgifte:</label>
-            <input type="date" id="loanDate" name="loan_date" class="form-control">
+            <input type="date" id="loanDate" name="loan_date" class="form-control" value="{{$loanInfo[2]}}" required>
             <label for="returnDate">datum van terugname:</label>
-            <input type="date" id="returnDate" name="return_date"  class="form-control">
+            <input type="date" id="returnDate" name="return_date"  class="form-control" value="{{$loanInfo[3]}}" required>
             <input type="submit" value="pas aan" class="btn">
         </div>
 
