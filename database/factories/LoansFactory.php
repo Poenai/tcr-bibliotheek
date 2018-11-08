e4db2beeb0cc6c25d0e11bd50e5da7c4bb1c165a
pietervanheijningen <pietervanheijningen@outlook.com>
2018-11-08 15:14:41 +0100

    improved loans seeder

--- database/factories/LoansFactory.php
+++ database/factories/LoansFactory.php
@@ -7,6 +7,6 @@ $factory->define(App\Loan::class, function () {
 		'user_id'     => \App\User::all()->random()->id,
 		'book_id'     => \App\Book::all()->random()->id,
 		'loan_date'   => Carbon::now(),
-		'return_date' => Carbon::now(),
+		'return_date' => Carbon::now()->addWeeks(2),
 	];
 });
